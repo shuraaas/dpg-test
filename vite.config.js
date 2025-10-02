631fa52bc4a4071a5d384b50a3bae0f27fc57229
Alexandr Perunov <sanyock.perunov@yandex.ru>
2025-10-02 11:31:26 +0500

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@constants': path.resolve(__dirname, 'src/constants'),
+        },
+    },
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        chunkSizeWarningLimit: 500,
+        outDir: 'build',
+    },
+});
