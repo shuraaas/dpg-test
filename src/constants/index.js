e10e8d8a10df5b35dc7a19c3df50553a9fa7e19c
Alexandr Perunov <sanyock.perunov@yandex.ru>
2025-10-02 14:43:27 +0500

    feat: add constants

---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+export const DAYS_LOCALE_RU = [
+  {
+    name: 'Пн',
+    isHidden: false,
+  },
+  {
+    name: 'Вт',
+    isHidden: true,
+  },
+  {
+    name: 'Ср',
+    isHidden: false,
+  },
+  {
+    name: 'Чт',
+    isHidden: true,
+  },
+  {
+    name: 'Пт',
+    isHidden: false,
+  },
+  {
+    name: 'Сб',
+    isHidden: true,
+  },
+  {
+    name: 'Вс',
+    isHidden: true,
+  },
+];
